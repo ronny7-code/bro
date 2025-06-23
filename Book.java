fc31a478b7c26c0cd9d5a3a6ca27f51934439abd
ronny7-code <bijay98813@gmail.com>
2025-06-23 02:16:39 -0500

    Changes in remove method

--- Book.java
+++ Book.java
@@ -7,7 +7,7 @@ Scanner scanner = new Scanner(System.in);
     private String author;
     private int ISBN;
     int noOfBooksToAdd;
-    int noOfBooksToRemove;
+    String nameOfBooksToRemove;
     int indexOfBook;
 
     ArrayList<Book> books = new ArrayList<>();
@@ -64,14 +64,23 @@ Scanner scanner = new Scanner(System.in);
     }
 
     public void remove(){
-        System.out.print("Enter the number of books you want to remove: ");
-        noOfBooksToRemove = scanner.nextInt();
+        // System.out.print("Enter the number of books you want to remove: ");
+        // noOfBooksToRemove = scanner.nextInt();
 
-        for (int i = 0; i < noOfBooksToRemove; i++) {
-            System.out.println("Enter the index of the book to remove from the collection: ");
-            indexOfBook = scanner.nextInt();
-            books.remove(indexOfBook);
+        // for (int i = 0; i < noOfBooksToRemove; i++) {
+        //     System.out.println("Enter the index of the book to remove from the collection: ");
+        //     indexOfBook = scanner.nextInt();
+        //     books.remove(indexOfBook);
+        // }
+
+        System.out.println("Enter the name of the book you want to delete: ");
+        nameOfBooksToRemove = scanner.nextLine();
+
+        for(int i = 0; i < books.length(); i++){
+            if(nameOfBooksToRemove.equals(i))
         }
+
+
     }
 
     public void collection(){
